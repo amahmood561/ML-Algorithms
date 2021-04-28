902ef75c18b17acca6b3a4f7fa989f4215390c73
DESKTOP-K2F6M1C\A_mah <amahmood561@gmail.com>
2021-04-27 21:14:09 -0400

    init

---
+++ LinearRegression.py
@@ -0,0 +1,27 @@
+from random import randint
+from sklearn.linear_model import LinearRegression
+
+class LinearRegression:
+
+    def __init__(self):
+        self.TRAIN_SET_LIMIT = 1000
+        self.TRAIN_SET_COUNT = 100
+
+    def run(self):
+        TRAIN_INPUT = list()
+        TRAIN_OUTPUT = list()
+        for i in range(self.TRAIN_SET_COUNT):
+            a = randint(0, self.TRAIN_SET_LIMIT)
+            b = randint(0, self.TRAIN_SET_LIMIT)
+            c = randint(0, self.TRAIN_SET_LIMIT)
+            op = a + (2*b) + (3*c)
+            TRAIN_INPUT.append([a, b, c])
+            TRAIN_OUTPUT.append(op)
+
+        predictor = LinearRegression(n_jobs=-1)
+        predictor.fit(X=TRAIN_INPUT, y=TRAIN_OUTPUT)
+
+        X_TEST = [[10, 20, 30]]
+        outcome = predictor.predict(X=X_TEST)
+        coefficients = predictor.coef_
+        print('Outcome : {}\nCoefficients : {}'.format(outcome, coefficients))
